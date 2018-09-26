a5eecbf10921cee4bda68421b723b211001fb064
Indira Williams <indirajamurath@gmail.com>
2018-09-26 11:21:16 -0400

    done

--- app/router.js
+++ app/router.js
@@ -6,6 +6,12 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('bricks', {path: '/legos'});
+  // dynamic route 'item/:item_name'
+  this.route('item', {path: 'item/:item_name'}, function() {
+    this.route('nutrition');
+  });
+
 });
 
 export default Router;
